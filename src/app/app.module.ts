648658d77ce77c103d96e72d0a4db50cd2e9becc
Osman Samad <osamad84@yahoo.com>
2020-02-18 15:09:16 -0600

    changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BeerComponent } from './component/beer/beer.component';
+import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/beer/beer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {IBeer} from '../../interface/ibeer';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import {BeerService} from '../../service/beer.service'
+
+@Component({
+  selector: 'app-beer',
+  templateUrl: './beer.component.html',
+  styleUrls: ['./beer.component.css']
+})
+export class BeerComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'img', 'name', 'tagline', 'description', 'abv'];
+  dataSource: MatTableDataSource<IBeer>;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(private beerService: BeerService ) { }
+
+  async ngOnInit() {
+    const data = await this.beerService.getAll();
+
+    this.dataSource= new MatTableDataSource(data);
+    this.dataSource.sort= this.sort;
+
+  }
+  applyFilter(filter: string){
+    this.dataSource.filter = filter.trim().toLowerCase();
+  }
+}
